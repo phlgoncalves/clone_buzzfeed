60906a8fb4fc868cc14c268b32b12f8a71ca239d
Pedro Goncalves <peedro.goncalves@gmail.com>
2025-08-19 22:10:44 -0300

    feat: created logic for component quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+  title: string = ''
 
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answerSelected: string = ''
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
 }
